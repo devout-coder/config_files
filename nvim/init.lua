6d1b6a689f222b496384b4f3cbd43323d0bb1dee
Pranil <awessssomepro@gmail.com>
2023-06-25 21:00:03 +0530

    Backup: Sun Jun 25 09:00:03 PM IST 2023

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,62 +1,21 @@
-local impatient_ok, impatient = pcall(require, "impatient")
-if impatient_ok then impatient.enable_profile() end
+require "core"
 
--- vim.api.nvim_set_option("clipboard","unnamedplus") 
-vim.opt.clipboard = 'unnamedplus'
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-for _, source in ipairs {
-  "core.utils",
-  "core.options",
-  "core.bootstrap",
-  "core.diagnostics",
-  "core.autocmds",
-  "core.mappings",
-  "configs.which-key-register",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+if custom_init_path then
+  dofile(custom_init_path)
 end
 
-require('toggleterm').setup {}
-require('neo-tree').setup {
-      filesystem = {
-          filtered_items = {
-            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-            hide_dotfiles = false,
-            hide_gitignored = false,
-          },
-      },
-}
+require("core.utils").load_mappings()
 
-function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
 end
 
-map("n", "pp", "$", { silent = true })
-map("n", "qq", "0", { silent = true })
-map("n", "<Esc>", ":noh<return><Esc>", { silent = true })
-map("v", "p", '"_dP')
-map("t","jj", "<C-\\><C-n>")
--- map("v", "p", '"_dP')
-
-astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
-
-if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
-  vim.schedule(function() astronvim.notify("Unsupported Neovim Version! Please check the requirements", "error") end)
-end
-
--- call plug#begin('~/.local/share/nvim/site/plugged')
--- Plug 'junegunn/goyo.vim'
--- call plug#end()
---
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin', '~/.local/share/nvim/site/plugged')
-
-Plug 'tpope/vim-fugitive'
-
-vim.call('plug#end')
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
