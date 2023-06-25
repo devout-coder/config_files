d62c5001fa76a8a5bcf701dc803599d3aa352950
Pranil <awessssomepro@gmail.com>
2023-06-25 15:00:01 +0530

    Backup: Sun Jun 25 03:00:01 PM IST 2023

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,15 @@ for _, source in ipairs {
 end
 
 -- my custom keybindings
+require('neo-tree').setup {
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = false,
+    },
+  }
+}
 
 function map(mode, lhs, rhs, opts)
     local options = { noremap = true }
